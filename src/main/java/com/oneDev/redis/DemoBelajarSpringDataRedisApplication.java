483df9409b87c06a768cfb125640c605fa50aa39
Ruswanda2020 <wandasukabumi2020@gmail.com>
2024-12-23 20:40:21 +0700

    🎉 First commit: Start Redis learning journey

---
+++ src/main/java/com/oneDev/redis/DemoBelajarSpringDataRedisApplication.java
@@ -0,0 +1,13 @@
+package com.oneDev.redis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoBelajarSpringDataRedisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoBelajarSpringDataRedisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/oneDev/redis/DemoBelajarSpringDataRedisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oneDev.redis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoBelajarSpringDataRedisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
